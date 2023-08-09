5cfa38dd39f17bce3a7ee0b4754618c7311ab244
NasiKecap51 <cl8455986@gmail.com>
2023-08-09 10:58:30 +0700

    tugas kasir

---
+++ kasiratk.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class cingcong {
+    public static void main(String[] args) {
+
+        double BukuTulis, Pensil, Penghapus, Penggaris;
+
+        BukuTulis = 2500;
+        Pensil = 2000;
+        Penghapus = 1500;
+        Penggaris = 1000;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Jumlah Beli BUku Tulis: ");
+        double JumlahBuku= input.nextDOuble();
+        double total=JumlahBuku*BukuTulis;
+      b    System.out.println("Masukan Pembayaran: ");
+        double bayar=inout.nextDouble();
+        double Diskon=0.05*total;
+        double kembali=bayar-(total-Diskon);
+        System.out.println("total Diskon: "+Diskon);
+        System.out.println("total Kembali: "+kembali);
+
+       
+
+    }}
+
+        
+
+       
+        
+        
+        
+        
+         
+    
+        
